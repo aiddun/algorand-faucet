461a3751a9f9557edb9f30336a07d9d161d655df
Aidan <aiddun@gmail.com>
2022-06-11 20:25:17 -0500

    Things

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,21 @@ function App() {
     <div className="App">
       <div className="flex justify-center items-center h-screen w-screen">
         <div
-          className=" shadow-sm md:shadow overflow-hidden rounded-lg 
+          className=" shadow overflow-hidden rounded-lg 
           border md:border-b md:border-l-0 md:border-r-0 md:border-t-0
-          border-gray-200 w-[20rem] w-full h-[20rem] flex flex-col"
+          border-gray-200 w-[20rem] h-[20rem] flex flex-col"
         >
-          <div className="mx-auto bg-gray-50 h-24 w-full flex flex-col justify-center align-middle">
+          <div className="mx-auto bg-gray-50 h-20 w-full flex flex-col justify-center align-middle">
             {/* <div className="mx-auto bg-white w-90 rounded-2xl shadow-sm text-center font-medium text-xl px-2 py-4"> */}
-              <h1 className="text-black align-middle  text-xl font-semibold"><span className="text-">UT Austin</span> Algorand Faucet</h1>
+              <h1 className="text-black align-middle   font-semibold text-[20pt] text-black"><span className="text-">UT</span> Algorand Faucet</h1>
             {/* </div> */}
           </div>
 
-          <div className="mx-auto  px-5 flex-grow  flex flex-col justify-center">
+          <div className="mx-auto  px-5 flex-grow  flex flex-col justify-center text-gray-900">
             <div className="text-lg">
               <p className="pb-2">To learn about Algorand, here's some for free you can use!</p>
               {/* <br className="m-0"/> */}
-              <span className="font-light my-0">
+              <span className="font-light my-0 text-gray-00">
                 <p>If you need a wallet, check out </p>
                 <a href="https://perawallet.app/" className=" text-blue-500">
                   Perra Wallet
--- src/Connect.tsx
+++ src/Connect.tsx
@@ -13,11 +13,13 @@ const SubmitButton = ({
   disabled,
   title = "Submit",
   loading,
+  error
 }: {
-  onClick?: () => {};
+  onClick?: () => void;
   disabled?: boolean;
   title: string;
   loading?: boolean;
+  error?: string
 }) => (
   // <span className="inline-flex rounded-md shadow-sm">
   //   <button
@@ -35,61 +37,69 @@ const SubmitButton = ({
       disabled={disabled}
       type="button"
       className={`inline-flex items-center px-3 py-1.5 border border-transparent 
-      text-xl leading-5 font-medium rounded-md text-white
-       ${
-         disabled
-           ? "bg-blue-600 opacity-70 cursor-default"
-           : `bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 
+      text-xl leading-5 font-medium rounded-md text-white shadow
+       ${disabled
+          ? "bg-blue-600 opacity-70 cursor-default"
+          : `bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 
               focus:shadow-outline-blue active:bg-blue-700 transition ease-in-out duration-150 `
-       }`}
+        }`}
       onClick={onClick}
     >
-      <div className="h-7 w-7">
-        {loading ? (
-          // Loading icon
-          <svg
-            class="animate-spin -ml-1 h-7 w-7 text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              class="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
+      {!error ?
+        <div className="h-7 w-7">
+          {loading ? (
+            // Loading icon
+            <svg
+              class="animate-spin -ml-1 h-7 w-7 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                class="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                class="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-7 w-7 -ml-1"
+              fill="none"
+              viewBox="0 0 24 24"
               stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
-            <path
-              class="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-7 w-7 -ml-1"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"
-            />
-          </svg>
-        )}
-      </div>
-      <p className="pl-0.5">{title}</p>
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"
+              />
+            </svg>
+          )}
+        </div>
+        : undefined}
+      <p className="pl-0.5">{error ?? title}</p>
     </button>
   </span>
 );
 
 const Connect = () => {
+
+
+  const [loading, setLoading] = useState(false);
+  const [txid, setTxid] = useState(null);
+  const [error, setError] = useState(null);
+
+
   const [connector, setConnector] = useState(() => {
     const connector = new WalletConnect({
       bridge: "https://bridge.walletconnect.org", // Required
@@ -107,8 +117,20 @@ const Connect = () => {
         throw error;
       }
 
+      setLoading(true)
+
       // Get provided accounts
       const { accounts } = payload.params[0];
+      const address = accounts[0];
+
+      fetch("https://ar-backend.aiddun.repl.co/api/tap", {
+        method: "POST", body: JSON.stringify({
+          "address": address
+        })
+      }).then((res) => res.json()).then(json => {
+        if (json.error) setError(`Sorry, ${json.error}`)
+        else if (json.txid) setTxid(json.txid)})
+
     });
 
     connector.on("session_update", (error, payload) => {
@@ -129,17 +151,22 @@ const Connect = () => {
     return connector;
   });
 
-  useEffect(() => {}, []);
 
   return (
     <div>
-      <SubmitButton
-        title={"Connect Wallet"}
-        onClick={() => {
-          connector.createSession();
-          
-        }}
-      />
+      {txid
+        ?
+        <p>Transaction confirmed! View it <a className="text-blue-700" href={`https://testnet.algoexplorer.io/tx/${txid}`}>here</a></p>
+        :
+        <SubmitButton
+          title={loading ? "Processing..." : "Connect Wallet"}
+          onClick={() => {
+            connector.createSession();
+          }}
+          loading={loading}
+          error={error}
+        />
+      }
       {/* <button onClick={() => connector.createSession()}>Connect Wallet</button> */}
     </div>
   );
